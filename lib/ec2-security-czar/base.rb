993eff782fb777fd380c41c1f3074f0f96c5f1ff
Ian Ehlert <ehlertij@gmail.com>
2014-05-02 16:58:15 -0500

    add mfa functionality

--- lib/ec2-security-czar/base.rb
+++ lib/ec2-security-czar/base.rb
@@ -5,10 +5,18 @@ module Ec2SecurityCzar
   class Base
     attr_accessor :ec2
 
-    def initialize
+    def initialize(mfa_token=nil)
       keys = YAML.load_file('config/aws_keys.yml')
       AWS.config(access_key_id: keys[:access_key], secret_access_key: keys[:secret_key], region: "us-east-1")
-      @ec2 = AWS.ec2
+      if keys[:mfa_serial_number]
+        @ec2 = mfa_auth(keys, mfa_token)
+      else
+        @ec2 = AWS.ec2
+      end
+    rescue Exception => e
+      puts e.class
+      puts e.message
+      exit 1
     end
 
     def update_rules
@@ -19,12 +27,24 @@ module Ec2SecurityCzar
     end
 
     def security_groups
-      ec2.security_groups.select{|sg| sg.name.match(security_group_matcher) }
+      ec2.security_groups
     end
 
     private
-    def security_group_matcher
-      /guardhouse-.*/
+    def mfa_auth(keys, mfa_token)
+      raise MFATokenMissing unless mfa_token
+      sts = AWS::STS.new(access_key_id: keys[:access_key], secret_access_key: keys[:secret_key])
+      session = sts.new_session(duration: 3600, serial_number: keys[:mfa_serial_number], token_code: mfa_token)
+      AWS::EC2.new(session.credentials)
+    rescue MFATokenMissing
+      puts "MFA token is required as an argument!"
+      exit 1
+    rescue AWS::STS::Errors::AccessDenied
+      puts "Multifactor Authentication failed."
+      exit 1
     end
+  end
+
+  class MFATokenMissing < StandardError
   end
 end
