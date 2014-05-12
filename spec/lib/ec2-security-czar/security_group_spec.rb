069393d2d84081613785786044831f6144863f8f
Andy Fleener <andrew.fleener@sportngin.com>
2014-05-12 11:30:15 -0500

    Update tests to ensure that the enviorment is passed properly, and adjust tests for the second environment argument

--- spec/lib/ec2-security-czar/security_group_spec.rb
+++ spec/lib/ec2-security-czar/security_group_spec.rb
@@ -25,16 +25,15 @@ module Ec2SecurityCzar
 
     let(:rules_config) { { outbound: [outbound_rule], inbound: [inbound_rule] } }
     let(:filename) { 'the/config/file' }
-    let(:file) { double("Raw File") }
+    let(:file) { "Raw File" }
     let(:parsed_file) { double("Parsed File") }
+    let(:environment) { 'environment' }
 
-    subject { SecurityGroup.new(api) }
+    subject { SecurityGroup.new(api, environment) }
 
     before do
       allow_any_instance_of(SecurityGroup).to receive(:config_filename).and_return(filename)
-      allow(YAML).to receive(:load).with(parsed_file).and_return(rules_config)
-      allow(ERB).to receive(:new).and_return(double(:result => parsed_file))
-      allow(File).to receive(:read).and_return(double(:result => file))
+      allow(File).to receive(:read).and_return(file)
       allow(File).to receive(:exists?).with(filename).and_return(true)
       allow(subject).to receive(:puts)
     end
@@ -46,6 +45,8 @@ module Ec2SecurityCzar
       let(:addition_outbound) { double("Outbound rule to be added") }
 
       before do
+        allow(ERB).to receive(:new).and_return(double(:result => parsed_file))
+        allow(YAML).to receive(:load).with(parsed_file).and_return(rules_config)
         allow(subject).to receive(:new_rules).with(:outbound).and_return([addition_outbound])
         allow(subject).to receive(:new_rules).with(:inbound).and_return([addition_inbound])
         allow(subject).to receive(:current_rules).with(:outbound).and_return([delete_outbound])
@@ -66,6 +67,15 @@ module Ec2SecurityCzar
         expect(addition_outbound).to receive(:authorize!).with(api)
         expect(addition_inbound).to receive(:authorize!).with(api)
         subject.update_rules
+      end
+    end
+
+    context "#load_rules" do
+      let(:environment) { 'parsed' }
+      let(:file) { "-- \n<%= environment %> \n" }
+
+      it "passes the environment into the erb rendering" do
+        expect(subject.send(:load_rules)).to match(environment)
       end
     end
 
