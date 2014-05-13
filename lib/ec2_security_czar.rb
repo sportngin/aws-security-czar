6db40de52383258be7b7ce36d29630f1d6c009e3
Ian Ehlert <ehlertij@gmail.com>
2014-05-13 14:54:16 -0500

    require version file

--- lib/ec2_security_czar.rb
+++ lib/ec2_security_czar.rb
@@ -1,3 +1,4 @@
 require_relative 'ec2-security-czar/base'
 require_relative 'ec2-security-czar/security_group'
 require_relative 'ec2-security-czar/rule'
+require_relative 'ec2-security-czar/version'
